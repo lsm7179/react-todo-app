dfb8c038981b79f9fabf7e89b9268692340d2bc3
이승민 <sml7179@naver.com>
2023-06-06 20:41:15 +0900

    feat: 할일 목록 UI만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+export default class App extends Component {
+    btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
+    }
 
-export default App;
+    getStyle = () => {
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: "none"
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="todoBlock">
+                    <div className="title">
+                        <h1>할일 목록</h1>
+                    </div>
+                    <div style={this.getStyle()}>
+                        <input type="checkbox" defaultChecked={false}/>
+                        공부하기
+                        <button style={this.btnStyle}>x</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
