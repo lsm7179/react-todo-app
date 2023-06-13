bdde57da18159f1f3c59cfe0561828a450938146
이승민 <sml7179@naver.com>
2023-06-13 21:10:26 +0900

    할 일 목록 부분을 위한 컴포넌트 생성하기(컴포넌트 분리하기)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, {useState} from "react";
 import './App.css';
+import List from "./components/List";
 
 export default function App() {
 
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
-
-    const btnStyle = {
-        color: "#fff", border: "none", padding: "5px 9px", borderRadius: "50%", cursor: "pointer", float: "right"
-    }
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px", borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? "line-through" : "none"
-        }
-    }
-
-    const handleClick = (id) => {
-        let newTodoData = todoData.filter((data) => data.id !== id)
-        setTodoData(newTodoData);
-    }
 
     const handleChange = (e) => {
         setValue(e.target.value);
@@ -41,27 +26,13 @@ export default function App() {
         setValue("");
     }
 
-    const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map(data => {
-            if (data.id === id) {
-                data.completed = !data.completed;
-            }
-            return data;
-        })
-        setTodoData(newTodoData);
-    }
 
     return (<div className="container">
         <div className="todoBlock">
             <div className="title">
                 <h1>할일 목록</h1>
             </div>
-            {this.state.todoData.map((data) => <div style={getStyle(data.completed)} key={data.id}>
-                <input type="checkbox" defaultChecked={false}
-                       onChange={() => handleCompleteChange(data.id)}/>
-                {data.title}
-                <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-            </div>)}
+            <List todoData={todoData} setTodoData={setTodoData}/>
             <form style={{display: 'flex'}} onSubmit={handleSubmit}>
                 <input type="text" name="value" style={{flex: '10', padding: '5px'}}
                        placeholder="해야 할 일을 입력하세요."
