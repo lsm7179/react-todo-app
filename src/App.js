4432cde5f412147b662cda4674657027bb49b3a1
이승민 <sml7179@naver.com>
2023-06-13 21:17:22 +0900

    Form 부분을 위한 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, {useState} from "react";
 import './App.css';
 import List from "./components/List";
+import Form from "./components/Form";
 
 export default function App() {
 
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    }
 
     const handleSubmit = (e) => {
         //페이지 리로드 되는것을 막아준다.
@@ -26,25 +23,13 @@ export default function App() {
         setValue("");
     }
 
-
     return (<div className="container">
         <div className="todoBlock">
             <div className="title">
                 <h1>할일 목록</h1>
             </div>
             <List todoData={todoData} setTodoData={setTodoData}/>
-            <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-                <input type="text" name="value" style={{flex: '10', padding: '5px'}}
-                       placeholder="해야 할 일을 입력하세요."
-                       value={value}
-                       onChange={handleChange}
-                />
-                <input type="submit"
-                       value="입력"
-                       className="btn"
-                       style={{flex: '1'}}
-                />
-            </form>
+            <Form value={value} setValue={setValue} handleSubmit={handleSubmit}/>
         </div>
     </div>);
 
